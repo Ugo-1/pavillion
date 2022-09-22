c0e0f8e8d6765217e5c436068b78a195dc053e5d
Ugochukwu <uumeh428@gmail.com>
2022-09-22 14:24:31 +0100

    Prevent rotation

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:pavillion/screens/nav.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
   runApp(const PavillionApp());
 }
 
